9703f27b67beb652dc10ae02c9a28060f6cfac2c
kvaltea <92518453+kvaltea@users.noreply.github.com>
2024-06-05 23:09:12 +0300

    index.js

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
 const { request } = require('express');
 const express = require('express')
 const mongoose = require('mongoose');
-const port = require || 4000;
 const Product = require('./models/product.model.js');
 const productRoute = require('./routes/product.route.js');
-const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000;
+const app = express();
+const uri = "mongodb+srv://kotobayk:GOe69L7leL5oeSg1@knowbase.awwglht.mongodb.net/?retryWrites=true&w=majority&appName=KnowBase";
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/api/products", productRoute);
-
-
-
-app.get('/', (req, res) => {
-    res.send("Hello, my name is Hello!");
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
-mongoose.connect("mongodb+srv://kotobayk:cyfUfutGQaLHA2Ze@cluster0.ztj7wpj.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0")
+
+mongoose.connect(uri)
     .then(() => {
         console.log("Connected to database!");
         app.listen(port, () => {
-            console.log("Server is running on port 4000");
+            console.log("Server is running on port 3000");
         });
     })
     .catch(() => {
         console.log("Connection failed!");
     });
+
+
+
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/products", productRoute);
+app.use("/api/products", Product);
+
+
+app.get('/', (req, res) => {
+    res.send("Hello, my name is Hello!");
+});
