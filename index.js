b6ead27cd094d46a5d47d0416c2f2d9f60e300ce
kvaltea <92518453+kvaltea@users.noreply.github.com>
2024-06-10 15:56:03 +0300

    index.js

--- index.js
+++ index.js
@@ -1,56 +1,26 @@
-const { request } = require('express');
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
-const Product = require('./models/product.model.js');
 const productRoute = require('./routes/product.route.js');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000;
 const app = express();
 const uri = "mongodb+srv://kotobayk:GOe69L7leL5oeSg1@knowbase.awwglht.mongodb.net/?retryWrites=true&w=majority&appName=KnowBase";
 
-
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
-
-async function run() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        await client.close();
-    }
-}
-run().catch(console.dir);
-
-
-mongoose.connect(uri)
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to database!");
         app.listen(port, () => {
-            console.log("Server is running on port 3000");
+            console.log("Server is running on port " + port);
         });
     })
-    .catch(() => {
-        console.log("Connection failed!");
+    .catch(err => {
+        console.error("Connection failed!", err);
     });
-
-
-
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/products", productRoute);
-app.use("/api/products", Product);
-
 
 app.get('/', (req, res) => {
     res.send("Hello, my name is Hello!");
